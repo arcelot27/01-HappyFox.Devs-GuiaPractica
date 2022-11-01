2d63ec732f17894af092c90071edbb4e86a01dff
Breimax21 <107445172+Breimax21@users.noreply.github.com>
2022-11-01 11:40:08 -0500

    changes

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const play = document.querySelector(".play-meow")
+
+play.addEventListener("click", () => {
+    console.log("ss")
+    const meow = document.querySelector(".meow")
+    meow.play()
+})
