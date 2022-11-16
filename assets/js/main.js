3b482691c2d0e67f1cc0269c2cb0cfebcd23d834
Adrian Mauricio Arandia Urrea <yasnoadriandatos@gmail.com>
2022-11-16 10:37:50 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 const play = document.querySelector(".play-meow")
 
 play.addEventListener("click", () => {
-    console.log("ss")
     const meow = document.querySelector(".meow")
     meow.play()
 })
